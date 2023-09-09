e81a3c69a78784702b9e3b33958ba44057c293a2
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-09 04:05:49 -0400

    add icons to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
 	import NavItem from '$lib/components/NavItem.svelte';
+	import Home from '$lib/icons/Home.svelte';
+	import Info from '$lib/icons/Info.svelte';
 	import '../app.postcss';
 </script>
 
 <nav class="flex">
-	<NavItem href="/" strict>home</NavItem>
-	<NavItem href="/about">about</NavItem>
+	<NavItem href="/" strict>
+		<div class="flex gap-2 items-center">
+			<Home />
+			home
+		</div>
+	</NavItem>
+	<NavItem href="/about">
+        <div class="flex gap-2 items-center">
+			<Info />
+			about
+		</div>
+    </NavItem>
 </nav>
 <hr />
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,29 @@
 <script>
-	import GitHub from "$lib/icons/GitHub.svelte";
-	import Instagram from "$lib/icons/Instagram.svelte";
-	import Spotify from "$lib/icons/Spotify.svelte";
-	import X from "$lib/icons/X.svelte";
-
+	import GitHub from '$lib/icons/GitHub.svelte';
+	import Instagram from '$lib/icons/Instagram.svelte';
+	import Spotify from '$lib/icons/Spotify.svelte';
+	import X from '$lib/icons/X.svelte';
 </script>
 
 <body>
-    <h1 class="mt-6">nikki!</h1>
-    <div class="flex gap-1 items-center">
-        <a href="https://x.com/eanuliambru" target="_blank" class="opacity-30">
-            <X />
-        </a>
-        <a href="https://github.com/philosophermurr" target="_blank" class="opacity-30">
-            <GitHub/>
-        </a>
-        <a href="https://instagram.com/eanulrambul" target="_blank" class="opacity-30">
-            <Instagram />
-        </a>
-        <a href="https://open.spotify.com/user/jwnrz7ojbbue22r56nlj7384d" target="_blank" class="opacity-30">
-            <Spotify />
-        </a>
-    </div>
-    <p>i'm nikki!</p>
+	<h1 class="mt-6">nikki!</h1>
+	<div class="flex gap-1 items-center">
+		<a href="https://x.com/eanuliambru" target="_blank" class="opacity-40">
+			<X />
+		</a>
+		<a href="https://github.com/philosophermurr" target="_blank" class="opacity-40">
+			<GitHub />
+		</a>
+		<a href="https://instagram.com/eanulrambul" target="_blank" class="opacity-40">
+			<Instagram />
+		</a>
+		<a
+			href="https://open.spotify.com/user/jwnrz7ojbbue22r56nlj7384d"
+			target="_blank"
+			class="opacity-40"
+		>
+			<Spotify />
+		</a>
+	</div>
+	<p>i'm nikki!</p>
 </body>
