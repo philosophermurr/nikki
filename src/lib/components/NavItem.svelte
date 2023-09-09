7c2540302c0af92fc8aef8f20b9a9e1149b7efbd
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-09 03:22:01 -0400

    style navbar

---
+++ src/lib/components/NavItem.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	export let href: string;
+	export let strict = false;
+
+	$: active = strict ? $page.url.pathname === href : $page.url.pathname.startsWith(href);
+</script>
+
+<div class="flex ml-2 md:ml-0 border-b-2 {active ? 'border-b-rose-500' : 'border-b-transparent'}">
+	<a class="btn" {href}>
+		<div class="flex items-center gap-2 {active ? 'font-bold' : 'font-normal text-gray-700'}">
+			<slot />
+		</div>
+	</a>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import NavItem from '$lib/components/NavItem.svelte';
+	import '../app.postcss';
+</script>
+
+<nav class="flex">
+	<NavItem href="/" strict>home</NavItem>
+	<NavItem href="/about">about</NavItem>
+</nav>
+<hr />
+<slot />
