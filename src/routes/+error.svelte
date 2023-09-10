3f7470540a9c496deffbd1d1854aa51835518fdc
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-10 12:49:40 -0400

    fix spacing for error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<body class="px-10 max-w-4xl mx-auto">
+<body class="max-w-4xl mx-auto">
 	<h1 class="mt-10">{$page.status}: {$page.error?.message.toLowerCase()}</h1>
 	<p>
 		try going back <a
