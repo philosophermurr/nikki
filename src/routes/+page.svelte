abfb6d35b863a1ae6b39853273b2fe918079b033
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-10 02:45:13 -0400

    make discord not bad

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,72 +32,68 @@
 <body class="px-10 max-w-4xl mx-auto">
 	<h1 class="mt-10">nikki &gt; you</h1>
 	{#if $data}
-		<div class="w-max bg-black bg-opacity-30 rounded-full pr-16">
-			<table class="mt-10">
-				<tr class="flex gap-7 items-center">
-					<td>
-						<div class="relative">
-							<img
-								src={'https://cdn.discordapp.com/avatars/' +
-									$data.discord_user.id +
-									'/' +
-									$data.discord_user.avatar}
-								alt="my discord avatar"
-								class="rounded-full h-32"
-							/>
-							<div
-								class={`absolute top-24 right-1 w-4 h-4 md:w-6 md:h-6 rounded-full ring-[6px] md:ring-6 ring-black ${getStatusColor(
-									$data.discord_status
-								)} group flex justify-center`}
-							/>
-						</div>
-					</td>
-					<td>
-						<div class="flex gap-2 items-center">
-							{#if $data.discord_user.display_name}
-								<h5>
-									{$data.discord_user.display_name}
-								</h5>
-								<h5 class="opacity-50 font-normal">
-									{$data.discord_user.username}
-								</h5>
-							{:else}
-								<h5>
-									{$data.discord_user.username}
-								</h5>
-							{/if}
-						</div>
-						<div class="flex gap-2 items-center">
-							{#if $data?.activities.find((a) => a.type === 4)}
-								{#if emoji}
-									<img
-										src={'https://cdn.discordapp.com/emojis/' + emoji}
-										alt="the custom emoji in my status"
-										class="h-5"
-									/>
-								{/if}
-								{customStatus}
-							{/if}
-						</div>
-						{#if $data?.activities.find((a) => a.type === 2)}
-							<div class="flex gap-2 items-center">
-								<p class="font-bold">
-									{app}
-								</p>
-								<p>&middot;</p>
-								<p>
-									{activity}
-								</p>
-								<p class="opacity-50">
-									{#if details}
-										{details}
-									{/if}
-								</p>
-							</div>
+		<div class="bg-zinc-900 flex rounded-full pr-16 mt-10">
+			<div class="flex items-center gap-5">
+				<div class="relative">
+					<img
+						src={'https://cdn.discordapp.com/avatars/' +
+							$data.discord_user.id +
+							'/' +
+							$data.discord_user.avatar}
+						alt="my discord avatar"
+						class="rounded-full h-32"
+					/>
+					<div
+						class={`absolute top-24 right-1 w-4 h-4 md:w-6 md:h-6 rounded-full ring-[6px] md:ring-6 ring-zinc-900 ${getStatusColor(
+							$data.discord_status
+						)} group flex justify-center`}
+					/>
+				</div>
+				<div>
+					<div class="flex gap-2 items-center">
+						{#if $data.discord_user.display_name}
+							<h5>
+								{$data.discord_user.display_name}
+							</h5>
+							<h5 class="opacity-50 font-normal">
+								{$data.discord_user.username}
+							</h5>
+						{:else}
+							<h5>
+								{$data.discord_user.username}
+							</h5>
 						{/if}
-					</td>
-				</tr>
-			</table>
+					</div>
+					<div class="flex gap-2 items-center">
+						{#if $data?.activities.find((a) => a.type === 4)}
+							{#if emoji}
+								<img
+									src={'https://cdn.discordapp.com/emojis/' + emoji}
+									alt="the custom emoji in my status"
+									class="h-5"
+								/>
+							{/if}
+							{customStatus}
+						{/if}
+					</div>
+					{#if $data?.activities.find((a) => a.type === 2)}
+						<div class="flex gap-2 items-center">
+							<p class="font-bold">
+								{app}
+							</p>
+							<p>&middot;</p>
+							<p>
+								{activity}
+							</p>
+							<p class="opacity-50">
+								{#if details}
+									{details}
+								{/if}
+							</p>
+						</div>
+					{/if}
+				</div>
+			</div>
 		</div>
 	{:else}
 		<p>Waiting to fetch Discord data...</p>
