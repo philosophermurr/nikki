4bfb13df39cc12ea9c95dca27e6c6f517104c338
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-10 12:48:24 -0400

    add transitions to navbar

--- src/lib/components/NavItem.svelte
+++ src/lib/components/NavItem.svelte
@@ -7,9 +7,17 @@
 	$: active = strict ? $page.url.pathname === href : $page.url.pathname.startsWith(href);
 </script>
 
-<div class="flex ml-2 md:ml-0 border-b-2 {active ? 'border-b-rose-500' : 'border-b-transparent'}">
+<div
+	class="flex ml-2 md:ml-0 border-b-2 transition-all {active
+		? 'border-b-rose-500'
+		: 'border-b-transparent'}"
+>
 	<a class="btn" {href}>
-		<div class="flex items-center gap-2 {active ? 'font-bold' : 'font-normal opacity-50'}">
+		<div
+			class="flex items-center gap-2 transition-all {active
+				? 'font-bold'
+				: 'font-normal opacity-50'}"
+		>
 			<slot />
 		</div>
 	</a>
