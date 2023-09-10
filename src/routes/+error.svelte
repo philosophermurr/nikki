cb36cea59e5f4d4a0a70f16e96e1e94bab53a136
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-10 15:06:43 -0400

    add transition on page load

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,7 +4,7 @@
 
 <body class="max-w-4xl mx-auto">
 	<h1 class="mt-10">{$page.status}: {$page.error?.message.toLowerCase()}</h1>
-	<p>
+	<p class="mt-4">
 		try going back <a
 			href="/"
 			class="text-rose-500 hover:text-rose-400 hover:font-bold transition-all">home</a
