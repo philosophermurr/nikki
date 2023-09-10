f86a4c1b72b7fcc5dc24b73e8d99d2beb1fec5fd
nikki <106131029+philosophermurr@users.noreply.github.com>
2023-09-10 03:13:06 -0400

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<body class="px-10 max-w-4xl mx-auto">
+	<h1 class="mt-10">error {$page.status}: {$page.error?.message.toLowerCase()}</h1>
+</body>
